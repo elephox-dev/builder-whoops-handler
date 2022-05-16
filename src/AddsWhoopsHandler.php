8b381e50f7c2c848722b2baac50bc446fc74b116
Ricardo Boss <contact@ricardoboss.de>
2022-05-17 01:00:04 +0200

    Removed global exception handler registration and added optional error handler registration in services

--- src/AddsWhoopsHandler.php
+++ src/AddsWhoopsHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Elephox\Builder\Whoops;
 
 use Elephox\DI\Contract\ServiceCollection;
+use Elephox\Support\Contract\ErrorHandler;
 use Elephox\Support\Contract\ExceptionHandler;
 use Whoops\Run as WhoopsRun;
 use Whoops\RunInterface as WhoopsRunInterface;
@@ -12,15 +13,13 @@ trait AddsWhoopsHandler
 {
 	abstract protected function getServices(): ServiceCollection;
 
-	public function addWhoops(bool $registerGlobal = true): void
+	public function addWhoops(bool $registerErrorHandler = true): void
 	{
 		$this->getServices()->addSingleton(WhoopsRunInterface::class, WhoopsRun::class);
 		$this->getServices()->addSingleton(ExceptionHandler::class, WhoopsExceptionHandler::class, replace: true);
 
-		if ($registerGlobal) {
-			set_exception_handler(function ($exception) {
-				$this->getServices()->requireService(ExceptionHandler::class)->handleException($exception);
-			});
+		if ($registerErrorHandler) {
+			$this->getServices()->addSingleton(ErrorHandler::class, WhoopsExceptionHandler::class, replace: true);
 		}
 	}
 }
